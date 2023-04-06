43b42076f552733ec15cffadbe83d2630f814154
Kyryll Kovalenko <kyryll.kovalenko@gmail.com>
2023-04-06 22:30:38 +0200

    add sentry replays

--- src/main.ts
+++ src/main.ts
@@ -22,10 +22,17 @@ Sentry.init({
     new Sentry.BrowserTracing({
       routingInstrumentation: Sentry.vueRouterInstrumentation(router),
       tracePropagationTargets: ['localhost', 'sars-cov-2-app.firebaseapp.com', /^\//]
-    })
+    }),
+    new Sentry.Replay()
   ],
   tracesSampleRate: 1.0,
-  release: 'covid19_website@1.0.1'
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+  release: 'covid19_website@1.0.2'
 })
 
 app.use(createPinia())
